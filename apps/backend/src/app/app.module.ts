e52128f87e0aef8b87e12dd409e3455e068a3cee
wasim2263 <abdullahwasim42@gmail.com>
2023-03-18 16:39:00 +0200

    added typeorm config in the app.module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JourneyModule } from './journey/journey.module';
 import { StationModule } from './station/station.module';
+import {Station} from "./station/entities/station.entity";
+import {Journey} from "./journey/entities/journey.entity";
 
 @Module({
-  imports: [JourneyModule, StationModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        Station,
+        Journey
+      ],
+      synchronize: true,
+    }),
+    JourneyModule,
+    StationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
