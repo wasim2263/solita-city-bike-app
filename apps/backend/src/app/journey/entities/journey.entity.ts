b8967f5cd1610291fbacf52aadf20d036545f446
wasim2263 <abdullahwasim42@gmail.com>
2023-03-20 01:28:36 +0200

    station and journey entities added with the fields and relations

--- apps/backend/src/app/journey/entities/journey.entity.ts
+++ apps/backend/src/app/journey/entities/journey.entity.ts
@@ -1,1 +1,19 @@
-export class Journey {}
+import {Column, Entity, ManyToOne} from "typeorm";
+import {BaseEntity} from "../../common/base.entity";
+import {Station} from "../../station/entities/station.entity";
+
+@Entity()
+export class Journey extends BaseEntity {
+  @Column('float', {default: 0, comment: 'duration in meter'})
+  covered_distance: number;
+  @Column('integer', {default: 0, comment: 'duration in seconds'})
+  duration: number;
+  @Column({type: 'timestamp'})
+  departed_at: Date;
+  @Column({type: 'timestamp'})
+  returned_at: Date;
+  @ManyToOne((type) => Station, (departure_station) => departure_station.departure_journeys)
+  departure_station: Station;
+  @ManyToOne((type) => Station, (return_station) => return_station.return_journeys)
+  return_station: Station;
+}
--- apps/backend/src/app/station/entities/station.entity.ts
+++ apps/backend/src/app/station/entities/station.entity.ts
@@ -1,1 +1,22 @@
-export class Station {}
+import {BaseEntity} from "../../common/base.entity";
+import {Column, Entity, OneToMany} from "typeorm";
+import {Journey} from "../../journey/entities/journey.entity";
+
+@Entity()
+export class Station extends BaseEntity {
+
+  @Column('bigint', {default: 0, unique: true, unsigned: true})
+  station_id: number;
+  @Column({length: 500})
+  name: string;
+  @OneToMany(
+    () => Journey,
+    (journey) => journey.departure_station,
+  )
+  departure_journeys: Journey[];
+  @OneToMany(
+    () => Journey,
+    (journey) => journey.return_station,
+  )
+  return_journeys: Journey[];
+}
