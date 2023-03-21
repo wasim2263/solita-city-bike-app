95b614ba697ea48a58ab4930444d1e4c0c4546a2
wasim2263 <abdullahwasim42@gmail.com>
2023-03-21 15:41:40 +0200

    added ValidationPipe to validate request data

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,11 +7,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import {ValidationPipe} from "./app/common/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
