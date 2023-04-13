02e08cc58ce1296e3824079f27e4a2fdcf292fd8
wasim2263 <abdullahwasim42@gmail.com>
2023-04-13 17:41:56 +0300

    added ts-ignore in file-upload-station.dto

--- apps/backend/src/app/station/dto/file-upload-stations.dto.ts
+++ apps/backend/src/app/station/dto/file-upload-stations.dto.ts
@@ -2,5 +2,7 @@ import {ApiProperty} from "@nestjs/swagger";
 
 export class FileUploadStationsDto{
   @ApiProperty({ type: 'string', format: 'binary', required: true })
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   file: Express.Multer.File
 }
