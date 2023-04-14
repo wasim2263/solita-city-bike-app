82e429bb7b2f41c75f4510fc3776c1ab1bf8319c
wasim2263 <abdullahwasim42@gmail.com>
2023-04-14 15:12:12 +0300

    added non empty property to create journty dto

--- apps/backend/src/app/journey/dto/create-journey.dto.ts
+++ apps/backend/src/app/journey/dto/create-journey.dto.ts
@@ -1,24 +1,30 @@
 import {JourneyInterface} from "../../../interfaces/journey-interface";
 import {ApiProperty} from "@nestjs/swagger";
-import {IsPositive} from "class-validator";
+import {IsNotEmpty, IsPositive} from "class-validator";
 
 export class CreateJourneyDto implements Omit<JourneyInterface, 'id'> {
 
   @ApiProperty()
+  @IsNotEmpty()
   departed_at: Date;
 
   @IsPositive()
   @ApiProperty({description: 'The ID of the return station. Must be a positive integer.'})
+  @IsNotEmpty()
   departure_station_id: number;
   @ApiProperty()
   @IsPositive()
+  @IsNotEmpty()
   duration: number;
   @ApiProperty()
   @IsPositive()
+  @IsNotEmpty()
   covered_distance: number;
   @ApiProperty({description: 'The ID of the return station. Must be a positive integer.'})
   @IsPositive()
+  @IsNotEmpty()
   return_station_id: number;
   @ApiProperty()
+  @IsNotEmpty()
   returned_at: Date;
 }
